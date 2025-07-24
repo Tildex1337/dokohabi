275a78fac7a53e458f175dac3159958c25eac0ef
codelotusr <smaliukasdeividas@gmail.com>
2025-07-24 17:56:45 +0300

    Docker config update|| Added air for hot-reload

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
 
-import "rsc.io/quote"
+	"github.com/jackc/pgx/v5"
+)
 
 func main() {
-	fmt.Println(quote.Go())
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
 }
